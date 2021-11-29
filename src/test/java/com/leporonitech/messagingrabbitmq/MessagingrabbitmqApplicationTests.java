ec77d74aa3df6a4062fe366e877931d7703690af
Leporoni <leporoni@hotmail.com>
2021-11-29 14:20:46 -0300

    Config pom and readme

---
+++ src/test/java/com/leporonitech/messagingrabbitmq/MessagingrabbitmqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leporonitech.messagingrabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagingrabbitmqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
