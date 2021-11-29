5e21e52b071a6e723e487eb76d79160a7e54c44d
Leporoni <leporoni@hotmail.com>
2021-11-29 14:30:50 -0300

    Created Receiver and added docker-compose.yml to config RabbitMQ Broker

--- src/main/java/com/leporonitech/messagingrabbitmq/MessagingrabbitmqApplication.java
+++ src/main/java/com/leporonitech/messagingrabbitmq/MessagingrabbitmqApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MessagingrabbitmqApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(MessagingrabbitmqApplication.class, args);
 	}
 
